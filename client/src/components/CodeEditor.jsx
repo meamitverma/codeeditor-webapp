11ce4e00cdd0eb0c2e437990cbfcd15aab897e8d
meamitverma <amit.verma.22080@gmail.com>
2024-05-04 22:55:47 +0530

    language selector

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -1,24 +1,31 @@
 import { Box } from "@chakra-ui/react"
 import { Editor } from '@monaco-editor/react'
 import { useRef, useState } from "react"
+import LanguageSelector from "./LanguageSelector"
 
 
 const CodeEditor = () => {
 
     const editorRef = useRef()
     const [value, setValue] = useState("")
+    const [language, setLanguage] = useState('javascript')
 
     const onMount = (editor) => {
         editorRef.current = editor
         editor.focus();
     }
 
+    const onSelect = (language) => {
+        setLanguage(language);
+    }
+
   return (
     <Box>
+        <LanguageSelector language={language} onSelect={onSelect} />
         <Editor 
             theme="vs-dark"
             height={"75vh"}
-            defaultLanguage="javascript"
+            language={language}
             defaultValue={"// Write your code here"}
             onMount={
                 onMount
